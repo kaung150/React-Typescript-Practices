1ba714315a15d6ae01f0a39710c682710c18e59f
kaung150 <kmkhant1500@gmail.com>
2023-07-15 19:54:10 +0630

    Episode 2.2

--- typescript/app.js
+++ typescript/app.js
@@ -1,7 +1,7 @@
-var add = function (a, b) {
-    return a + b;
-};
-var num1 = 22;
-var num2 = 21;
-var result = add(num1, num2);
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 2.8;
+var result = add(number1, number2);
 console.log(result);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,13 +1,9 @@
-function add(n1: number, n2: number ){
-  
-    // if(typeof n1 !== 'number' && typeof n2 !== 'number'){
-    //     throw new Error("Wrong type passed")
-    // }
+function add(n1: number, n2: number){
     return n1 + n2;
 }
 
-const number1 = 6;
+const number1 = 5;
 const number2 = 2.8;
 
 const result = add(number1, number2);
-console.log(result);
+console.log(result)
