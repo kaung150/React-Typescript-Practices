606b6be31e1a1161d1fe6c2355640c358b30c262
kaung150 <kmkhant1500@gmail.com>
2023-07-16 08:35:30 +0630

    2.9 Array Types

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number
+// } 
 var person = {
     name: "Kaung Min Khant",
-    age: 30
+    age: 30,
+    hobbies: ['Sport', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ["Sport"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,19 @@
-const person: {
-    name: string;
-    age: number
-} = {
+// const person: {
+//     name: string;
+//     age: number
+// } 
+
+const person = {
     name: "Kaung Min Khant",
-    age: 30
+    age: 30,
+    hobbies: ['Sport', 'Cooking']
 };
 
+let favoriteActivities:string[];
+favoriteActivities = ["Sport"]
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
