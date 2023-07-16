ba13e131f447404bbbc8290363ac725995885501
kaung150 <kmkhant1500@gmail.com>
2023-07-16 07:29:55 +0630

    2.7 Object Types

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,20 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase:string){
-    
-    const result = n1 + n2;
-    
-    if(showResult){
-        console.log(phrase + result)
-    }else{
-        return n1 + n2;
-    }   
-}
+const person: {
+    name: string;
+    age: number
+} = {
+    name: "Kaung Min Khant",
+    age: 30
+};
 
-
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
-
+console.log(person.name);
